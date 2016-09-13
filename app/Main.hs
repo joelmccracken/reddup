a6c99173b5562e9c7d0e4fdcdf28f8e32d9d91f4
Joel McCracken <mccracken.joel@gmail.com>
2016-09-13 17:23:57 -0400

    starting to use option parser for options

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,34 @@ import Turtle
 import Data.Text
 import System.Directory
 import System.Posix.IO
+import Prelude hiding (FilePath)
+import System.IO
+
+-- parser :: Parser (FilePath, FilePath)
+-- parser = (,) <$> argPath "src" "the source file"
+--              <*> argPath "dest" "the destination file"
+
+
+data BackupConfigOption =
+  Directory String
+  deriving (Show, Read)
 
 
 main :: IO ()
 main = do
+  -- (src, dest) <- options "a simple `cp` script" parser
   home <- getHomeDirectory
+
+  handle <- openFile (home ++ "/haskell-backup.hsd") ReadMode
+  contents <- hGetContents handle
+  return ((read contents)::[BackupConfigOption])
+
   (r, w) <- createPipe
   fdWrite w "HITHERE\n"
   closeFd w
+
+
+
 
   home <- getHomeDirectory
   shell (
