f672b1e2213a36934b0ef6ddb53bc75e9165f290
Joel McCracken <mccracken.joel@gmail.com>
2016-12-11 16:19:35 -0500

    fuss with code organization

--- app/Main.hs
+++ app/Main.hs
@@ -63,29 +63,46 @@ gitStatus = do
   let statusStream = inshell "git status --porcelain" Turtle.empty
   fmap parseGitStatusLine statusStream
 
-gitTrackables :: String -> [Trackable]
-gitTrackables homeDir =
-  Prelude.map (Git . fromString)
-    [ homeDir ++ "/EF/"
-    , homeDir ++ "/Reference/"
-    , homeDir ++ "/Projects/git-stuff/"
-    ]
+gitTrackables :: [String] -> [Trackable]
+gitTrackables directories =
+  Prelude.map gitTrackable directories
 
-inboxTrackables :: String -> [Trackable]
-inboxTrackables homeDir =
-  [ InboxDir . fromString $ homeDir ++ "/Inbox/" ]
+gitTrackable :: String -> Trackable
+gitTrackable = Git . fromString
+
+inboxTrackables :: [String] -> [Trackable]
+inboxTrackables = Prelude.map inboxTrackable
+
+inboxTrackable :: String -> Trackable
+inboxTrackable = InboxDir . fromString
 
 directoriesToCheck :: Shell [Trackable]
 directoriesToCheck = do
   homeDir <- liftIO $ getHomeDirectory
-  return $ gitTrackables homeDir ++ inboxTrackables homeDir
+  return $ directoriesConfig homeDir
+
+directoriesConfig homeDir =
+  let
+    toHomeDir = Prelude.map (homeDir ++)
+    gitRepos =
+      toHomeDir [ "/EF/"
+                , "/Reference/"
+                , "/Projects/git-stuff/"
+                ]
+    inboxDirs =
+      toHomeDir [ "/Inbox", "/Desktop" ]
+  in
+    gitTrackables gitRepos ++ inboxTrackables inboxDirs
 
 handleTrackable :: Trackable -> Shell ()
 handleTrackable (Git dir) = do
   liftIO $ putStrLn $ "checking " ++ show dir
   cd dir
-  let status = gitStatus
-  view status
+  dirExists <- testdir ".git"
+  if dirExists then
+    liftIO checkGitStatus
+  else
+    liftIO $ putStrLn $ "ERROR IS NOT GIT REPO"
   return ()
 
 handleTrackable (InboxDir dir) = do
@@ -94,6 +111,11 @@ handleTrackable (InboxDir dir) = do
   let status = ls "."
   view status
   return ()
+
+checkGitStatus :: IO ()
+checkGitStatus = do
+  let status = gitStatus
+  view status
 
 main :: IO ()
 main = sh (do
