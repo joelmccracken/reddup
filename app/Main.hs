bd767a35a994c35391d2ad33a90f9c61a56176d9
Joel McCracken <mccracken.joel@gmail.com>
2016-12-14 12:31:12 -0500

    factor git stuff into a Git module

--- app/Main.hs
+++ app/Main.hs
@@ -2,66 +2,16 @@
 
 module Main where
 
-import Turtle hiding ((<|>))
 import Data.Text
 import System.Directory
 import Prelude hiding (FilePath)
-import Text.ParserCombinators.Parsec as Parsec
-
-data GitStatusType
-  = Staged Text
-  | Unstaged Text
-  | Untracked Text
-  | Unknown Text
-  deriving (Show)
+import Turtle hiding ((<|>))
+import Git
 
 data Trackable
   = Git Turtle.FilePath
   | InboxDir Turtle.FilePath
   deriving (Show)
-
-gitStatusLineParser :: Parsec.Parser GitStatusType
-gitStatusLineParser = do
-  parseUntracked
-    <|> parseStaged
-    <|> parseUnstaged
-
-parseUntracked :: Parsec.Parser GitStatusType
-parseUntracked = do
-  _ <- string "??"
-  value <- parseValueAfterLabel
-  return $ Untracked (fromString value)
-
-parseStaged :: Parsec.Parser GitStatusType
-parseStaged = do
-  _ <- string "M "
-  value <- parseValueAfterLabel
-  return $ Staged (fromString value)
-
-parseUnstaged :: Parsec.Parser GitStatusType
-parseUnstaged = do
-  _ <- string " M"
-  value <- parseValueAfterLabel
-  return $ Unstaged (fromString value)
-
-parseValueAfterLabel :: Parsec.Parser String
-parseValueAfterLabel = do
-  _ <- Parsec.space
-  Parsec.many Parsec.anyChar
-
-parseGitStatusLine :: Turtle.Line -> GitStatusType
-parseGitStatusLine line =
-  let
-    parsed = parse gitStatusLineParser "git status --porcelain" ((unpack . lineToText) line)
-  in
-    case parsed of
-      Left _ -> Unknown (lineToText line)
-      Right x -> x
-
-gitStatus :: Shell GitStatusType
-gitStatus = do
-  let statusStream = inshell "git status --porcelain" Turtle.empty
-  fmap parseGitStatusLine statusStream
 
 gitTrackables :: [String] -> [Trackable]
 gitTrackables directories =
@@ -69,6 +19,7 @@ gitTrackables directories =
 
 gitTrackable :: String -> Trackable
 gitTrackable = Git . fromString
+
 
 inboxTrackables :: [String] -> [Trackable]
 inboxTrackables = Prelude.map inboxTrackable
