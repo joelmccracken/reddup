09e89b53059fe827e632b190c50dbd161ecd4fca
Joel McCracken <mccracken.joel@gmail.com>
2018-08-30 01:50:41 -0400

    start parsing handler specs
    these are to add customizability, so that as things are being handled, custom
    cmds can be added.

--- src/Config.hs
+++ src/Config.hs
@@ -15,28 +15,67 @@ import qualified ShellUtil
 import Prelude -- Ensure Applicative is in scope and we have no warnings, before/after AMP.
 
 data Config =
-  Config {
-    locations :: [LocationSpec]
-  } deriving (Eq, Show)
+  Config
+    { locations :: [LocationSpec]
+    , handlers :: HandlerSpecs
+    } deriving (Eq, Show)
 
 data LocationSpec =
-  LocationSpec {
-    _type    :: Text
-  , location :: Text
-  } deriving (Eq, Show)
+  LocationSpec
+    { _type    :: Text
+    , location :: Text
+    } deriving (Eq, Show)
+
+data HandlerSpecs =
+  HandlerSpecs
+    { inboxHandlers :: InboxHandlerSpec
+    } deriving (Eq, Show)
+
+data InboxHandlerSpec =
+  InboxHandlerSpec
+    { commands :: [InboxHandlerCommandSpec]
+    } deriving (Eq, Show)
+
+
+data InboxHandlerCommandSpec =
+  InboxHandlerCommandSpec
+    { name :: Text
+    , cmd  :: Text
+    } deriving (Eq, Show)
+
 
 instance FromJSON Config where
   parseJSON (Y.Object v) =
     Config <$>
-    v .:   "locations"
-  parseJSON _ = fail "error parsing"
+      v .:   "locations" <*>
+      v .:   "handlers"
+  parseJSON _ = fail "error parsing config"
 
 instance FromJSON LocationSpec where
   parseJSON (Y.Object v) =
     LocationSpec <$>
     v .:   "type"   <*>
     v .:   "location"
-  parseJSON _ = fail "error parsing"
+  parseJSON _ = fail "error parsing location specs"
+
+instance FromJSON HandlerSpecs where
+  parseJSON (Y.Object v) =
+    HandlerSpecs <$>
+    v .: "inbox"
+  parseJSON _ = fail "error parsing handler specs"
+
+instance FromJSON InboxHandlerSpec where
+  parseJSON (Y.Object v) =
+    InboxHandlerSpec <$>
+      v .: "commands"
+  parseJSON _ = fail "error parsing inbox handler spec"
+
+instance FromJSON InboxHandlerCommandSpec where
+  parseJSON (Y.Object v) =
+    InboxHandlerCommandSpec <$>
+      v .: "name" <*>
+      v .: "cmd"
+  parseJSON _ = fail "error parsing inbox handler command"
 
 getConfigFilename :: Tu.Shell SIO.FilePath
 getConfigFilename = fmap (Tu.fromString . T.unpack . Tu.lineToText) (ShellUtil.expandGlob "~/.reddup.yml")
