ee0b647f477c3d33efacd9c0895a4efba38e6d2f
Joel McCracken <mccracken.joel@gmail.com>
2019-12-07 19:14:39 -0500

    fix some stuff (handling warnings)

--- src/GitParse.hs
+++ src/GitParse.hs
@@ -1,7 +1,14 @@
-module GitParse where
+module GitParse ( parseGitBranchLine
+                , parseGitStatusLine
+                , GitStatus(..)
+                , GitBranch(..)
+                ) where
+
+import Prelude (Show, Eq, Either(..), String, ($), return, (>>), (.))
 
 import Data.Text
-import Text.ParserCombinators.Parsec as Parsec
+import qualified Text.ParserCombinators.Parsec as P
+import Text.ParserCombinators.Parsec ((<|>), try)
 import Turtle hiding ((<|>))
 
 newtype GitBranch
@@ -19,7 +26,7 @@ data GitStatus
   | Unknown Text
   deriving (Show, Eq)
 
-gitStatusLineParser :: Parsec.Parser GitStatus
+gitStatusLineParser :: P.Parser GitStatus
 gitStatusLineParser = do
   try parseUntracked
     <|> try parseAdded
@@ -29,67 +36,68 @@ gitStatusLineParser = do
     <|> try parseDeleted
     <|> try parseUnstaged
 
-parseUntracked :: Parsec.Parser GitStatus
+parseUntracked :: P.Parser GitStatus
 parseUntracked = do
-  _ <- string "??"
+  _ <- P.string "??"
   value <- parseValueAfterLabel
   return $ Untracked (fromString value)
 
-parseAdded :: Parsec.Parser GitStatus
+parseAdded :: P.Parser GitStatus
 parseAdded = do
-  _ <- string "A "
+  _ <- P.string "A "
   value <- parseValueAfterLabel
   return $ Added (fromString value)
 
-parseAddedAndModified :: Parsec.Parser GitStatus
+parseAddedAndModified :: P.Parser GitStatus
 parseAddedAndModified = do
-  _ <- string "AM"
+  _ <- P.string "AM"
   value <- parseValueAfterLabel
   return $ AddedAndModified (fromString value)
 
-parseStaged :: Parsec.Parser GitStatus
+parseStaged :: P.Parser GitStatus
 parseStaged = do
-  _ <- string "M "
+  _ <- P.string "M "
   value <- parseValueAfterLabel
   return $ Staged (fromString value)
 
-parseStagedAndUnstaged :: Parsec.Parser GitStatus
+parseStagedAndUnstaged :: P.Parser GitStatus
 parseStagedAndUnstaged = do
-  _ <- string "MM"
+  _ <- P.string "MM"
   value <- parseValueAfterLabel
   return $ StagedAndUnstaged (fromString value)
 
-parseDeleted :: Parsec.Parser GitStatus
+parseDeleted :: P.Parser GitStatus
 parseDeleted = do
-  _ <- string " D"
+  _ <- P.string " D"
   value <- parseValueAfterLabel
   return $ Deleted (fromString value)
 
-parseUnstaged :: Parsec.Parser GitStatus
+parseUnstaged :: P.Parser GitStatus
 parseUnstaged = do
-  _ <- string " M"
+  _ <- P.string " M"
   value <- parseValueAfterLabel
   return $ Unstaged (fromString value)
 
-parseValueAfterLabel :: Parsec.Parser String
+parseValueAfterLabel :: P.Parser String
 parseValueAfterLabel = do
-  _ <- Parsec.space
-  Parsec.many Parsec.anyChar
+  _ <- P.space
+  P.many P.anyChar
 
 parseGitStatusLine :: Turtle.Line -> GitStatus
 parseGitStatusLine line =
   let
-    parsed = parse gitStatusLineParser "git status --porcelain" ((unpack . lineToText) line)
+    parsed = P.parse gitStatusLineParser "git status --porcelain" ((unpack . lineToText) line)
   in
     case parsed of
       Left _ -> Unknown (lineToText line)
       Right value -> value
 
-gitBranchLineParser :: Parsec.Parser GitBranch
+gitBranchLineParser :: P.Parser GitBranch
 gitBranchLineParser =
   let
-    spacesThenAnything = (Parsec.spaces >> Parsec.many Parsec.anyChar)
-    branchNameParser = (string "*" >> spacesThenAnything)
+    spacesThenAnything = (P.spaces >> P.many P.anyChar)
+    branchNameParser :: P.Parser String
+    branchNameParser = (P.string "*" >> spacesThenAnything)
              <|> spacesThenAnything
   in do
     name <- branchNameParser
@@ -98,7 +106,7 @@ gitBranchLineParser =
 parseGitBranchLine :: Turtle.Line -> GitBranch
 parseGitBranchLine line =
   let
-    parsed = parse gitBranchLineParser "git branch" ((unpack . lineToText) line)
+    parsed = P.parse gitBranchLineParser "git branch" ((unpack . lineToText) line)
   in
     case parsed of
       Left _ -> GitBranch (lineToText line)
